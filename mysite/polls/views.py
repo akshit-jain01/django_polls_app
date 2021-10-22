f41d1f9b0d6ad702deb65b941178e5aeff715ec4
akshit-jain01 <akshitjain.1112003@gmail.com>
2021-10-22 13:41:00 +0530

    Added Admin page settings and bg image

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 # def index(request):
@@ -14,7 +15,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
